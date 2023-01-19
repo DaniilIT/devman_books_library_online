615b363bd9da76d07792df2a8c6f560472970cd9
Daniil_Manuilov <manuilovdaniil@yandex.ru>
2023-01-19 20:02:20 +0100

    Integrate livereload

--- render_website.py
+++ render_website.py
@@ -1,10 +1,10 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import json
-import os
+from livereload import Server, shell
+# import os
 
 
-def main():
+def on_reload():
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html'])
@@ -19,8 +19,13 @@ def main():
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
 
-    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-    server.serve_forever()
+
+def main():
+    on_reload()
+
+    server = Server()
+    server.watch('template.html', on_reload)
+    server.serve(root='.')
 
 
 if __name__ == "__main__":
@@ -28,13 +33,13 @@ if __name__ == "__main__":
     # mb = []
     # for file_ in os.listdir('./data/books'):
     #     mb.append(''.join(file_.split('.')[1:-1])[1:])
-    #
+
     # with open('data/books.json', "r") as json_file:
     #     books = json.load(json_file)
-    #
+
     # for book in books:
     #     if book['title'] not in mb:
     #         books.remove(book)
-    #
+
     # with open('data/books.json', "w") as json_file:
     #     json.dump(books, json_file, indent=2, ensure_ascii=False)
