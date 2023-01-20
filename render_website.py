a0e34fc70dd03f14003fab75d5a847cd64cfde5f
Daniil_Manuilov <manuilovdaniil@yandex.ru>
2023-01-20 10:06:19 +0100

    Add links to books txt files

--- render_website.py
+++ render_website.py
@@ -2,7 +2,6 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 import json
 from livereload import Server
 from more_itertools import chunked
-# import os
 
 
 def on_reload():
@@ -31,16 +30,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    # mb = []
-    # for file_ in os.listdir('./data/books'):
-    #     mb.append(''.join(file_.split('.')[1:-1])[1:])
-
-    # with open('data/books.json', "r") as json_file:
-    #     books = json.load(json_file)
-
-    # for book in books:
-    #     if book['title'] not in mb:
-    #         books.remove(book)
-
-    # with open('data/books.json', "w") as json_file:
-    #     json.dump(books, json_file, indent=2, ensure_ascii=False)
